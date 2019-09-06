1a781df3517b29a78b2b3520d519a9ffbb279819
donomii <donomii@gmail.com>
2019-09-06 21:00:42 +0200

    Update udplib.go

--- udplib.go
+++ udplib.go
@@ -152,7 +152,7 @@ func StartUdp(hostName, portNum string, processor func(incoming, outgoing chan U
 	return incoming, outgoing
 }
 
-//_SendMessage_ sends _data_ to _server_, via _outchan_.  You get _outchan_ passed to the _processor_ function, that you provide to StartUdp
+// *SendMessage* sends _data_ to _server_, via _outchan_.  You get _outchan_ passed to the _processor_ function, that you provide to StartUdp
 func SendMessage(outchan chan UdpMessage, data []byte, server string, port int) {
 	outchan <- UdpMessage{data, server, port, getSequence(), "", time.Now()}
 }
